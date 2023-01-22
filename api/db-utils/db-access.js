67c0539f2288966578eaeb451073d3ee924d92de
Liu Santana <lsantana8@boscdelacoma.cat>
2023-01-23 00:14:52 +0100

    treballador(dni) + guardiesxTreballador

--- api/db-utils/db-access.js
+++ api/db-utils/db-access.js
@@ -17,7 +17,19 @@ const getPlantilla = (async (req, res) => {
         })
 }
 )
-
+const getTreballador = (async (req, res) => {
+    const DNI = req.query.dni;
+    connection.query("select * from treballador where dni = '"+DNI+"'", (err, result) => {
+        if (err) {
+            res.status(400).send('Error al obtenir treballdors')
+        }
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+        res.status(200).send({
+            "resultat": { "dades": result }
+        })
+    })
+});
 const getTreballadors = (async (req, res) => {
     
     connection.query("select * from treballador where estat = 'actiu' " , (err, result) => {
@@ -45,7 +57,26 @@ const getCategories = (async (req, res) => {
     })
 });
 
+const getGuardiesTreballador = (async (req, res) => {
+    const DADES_GuardiesTreballador = 'gx.id, gx.estat_guardia, gx.estat';
+    const DADES_Guardia = ',g.id, g.data_guardia, g.unitat, g.torn, g.categoria';
+
+    const DADES = DADES_GuardiesTreballador+" "+DADES_Guardia;
+    const DNI = req.query.dni;
+    let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat = 'actiu' and gx.dni_treballador ='"+DNI+"'";
+    connection.query(sql, (err, result) => {
+        if (err) {
+            res.status(400).send('Error al obternir guardes per treballador')
+        }
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+        res.status(200).send({
+            "resultat": { "dades": result }
+        });
+    })
+});
 
 
-module.exports = { getPlantilla, getTreballadors, getCategories };
+
+module.exports = { getPlantilla, getTreballadors, getTreballador, getCategories, getGuardiesTreballador };
 
--- api/routes/data-access.js
+++ api/routes/data-access.js
@@ -5,7 +5,9 @@ const db_access = require('../db-utils/db-access')
 
 router.get('/plantillaGuardia', db_access.getPlantilla) 
 router.get('/treballadors' , db_access.getTreballadors)
+router.get('/treballador' , db_access.getTreballador)
 router.get('/categories' , db_access.getCategories)
+router.get('/guardiestreballador' , db_access.getGuardiesTreballador)
 
 
 
