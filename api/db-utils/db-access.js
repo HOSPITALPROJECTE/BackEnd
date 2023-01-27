1406fe22e0c4b945a13e3383c9d66ba34f7c4973
razzouko <100167727+razzouko@users.noreply.github.com>
2023-01-27 16:23:46 +0100

    back actualitzat amb els canvis de tots

--- api/db-utils/db-access.js
+++ api/db-utils/db-access.js
@@ -16,6 +16,7 @@ const getPlantilla = (async (req, res) => {
             })
         })
 });
+
 const getTreballador = (async (req, res) => {
     const DNI = req.query.dni;
     connection.query("select * from treballador where dni = '"+DNI+"' and estat = 'actiu'", (err, result) => {
@@ -29,6 +30,7 @@ const getTreballador = (async (req, res) => {
         })
     })
 });
+
 const getTreballadors = (async (req, res) => {
     
     connection.query("select * from treballador where estat != 'eliminat' " , (err, result) => {
@@ -69,18 +71,8 @@ const getTorns = (async (req, res) => {
     })
 });
 
-const getUnitats = (async (req, res) => {
-    connection.query("select nom from unitat where estat != 'eliminat'", (err, result) => {
-        if (err) {
-            res.status(400).send('Error al obternir categoria')
-        }
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
-        res.status(200).send({
-            "resultat": { "dades": result }
-        });
-    })
-});
+
+
 const getFestius = (async (req, res) => {
     connection.query("select * from festius where estat != 'eliminat'", (err, result) => {
         if (err) {
@@ -99,7 +91,6 @@ const getGuardiesTreballador = (async (req, res) => {
     const DADES_Guardia = ',g.id, g.data_guardia, g.unitat, g.torn, g.categoria';
 
     const DADES = DADES_GuardiesTreballador+" "+DADES_Guardia;
-<<<<<<< Updated upstream
     const DNI = req.query.dni;
     let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat != 'eliminat' and gx.dni_treballador ='"+DNI+"' order by g.data_guardia";
     connection.query(sql, (err, result) => {
@@ -119,14 +110,9 @@ const getAgendaTreballador = (async (req, res) => {
     const DADES_Guardia = ',g.id, g.data_guardia, g.unitat, g.torn, g.categoria';
 
     const DADES = DADES_GuardiesTreballador+" "+DADES_Guardia;
-    const DNI = req.query.dni;
     const DATA = req.query.data;
-
-    let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat != 'eliminat' and gx.dni_treballador ='"+DNI+"' and g.data_guardia > '"+DATA+"' order by g.data_guardia";
-=======
     const DNI = req.body.dni;
     let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat = 'actiu' and gx.dni_treballador ='"+DNI+"'";
->>>>>>> Stashed changes
     connection.query(sql, (err, result) => {
         if (err) {
             res.status(400).send('Error al obternir guardes per treballador')
@@ -139,7 +125,6 @@ const getAgendaTreballador = (async (req, res) => {
     })
 });
 
-<<<<<<< Updated upstream
 const getHistorialTreballador = (async (req, res) => {
     const DADES_GuardiesTreballador = 'gx.id, gx.estat_guardia, gx.estat';
     const DADES_Guardia = ',g.id, g.data_guardia, g.unitat, g.torn, g.categoria';
@@ -150,13 +135,18 @@ const getHistorialTreballador = (async (req, res) => {
 
     let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat != 'eliminat' and gx.dni_treballador ='"+DNI+"' and g.data_guardia < '"+DATA+"' order by g.unitat";
     connection.query(sql, (err, result) => {
-=======
-const getUnitats = (async (req, res) => {
+        let sql = "select "+ DADES +" from guardies_x_treballador as gx JOIN guardies as g on gx.id = g.id where gx.estat != 'eliminat' and gx.dni_treballador ='"+DNI+"' and g.data_guardia < '"+DATA+"' order by g.unitat";
+            if (err) {
+                res.status(400).send('Error al obternir guardes per treballador')
+            }
+    })
+});
 
-    connection.query("select * from unitat where estat = 'actiu'", (err, result) => {
->>>>>>> Stashed changes
+
+const getUnitats = (async (req, res) => {
+    connection.query("select nom from unitat where estat != 'eliminat'", (err, result) => {
         if (err) {
-            res.status(400).send('Error al obternir guardes per treballador')
+            res.status(400).send('Error al obternir categoria')
         }
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
@@ -166,7 +156,6 @@ const getUnitats = (async (req, res) => {
     })
 });
 
-<<<<<<< Updated upstream
 const updateFestiu = (async (req, res) => {
     let sql = `UPDATE festius SET estat = ? WHERE dia = ?`;
     connection.query(sql,[req.body.status,req.body.dia], (error, results, fields) => {
@@ -174,6 +163,7 @@ const updateFestiu = (async (req, res) => {
       res.send({"d":req.body.status});
     });
 });
+
 const insertFestiu = (async (req, res) => {
     try {
         let sql = `INSERT into festius(dia, estat) VALUES (?, ?)`;
@@ -198,8 +188,6 @@ const insertFestiu = (async (req, res) => {
 });
 
 
-module.exports = { getPlantilla, getTreballadors, getTreballador, getCategories, getGuardiesTreballador, getAgendaTreballador, getTorns, getUnitats,getHistorialTreballador,getFestius,updateFestiu, insertFestiu };
-=======
 const getGuardiesMesAny = (async (req, res) => {
 
     connection.query("SELECT * FROM hospital.guardies where data_guardia between ? and ? order by data_guardia asc;", [req.body.primer_dia , req.body.ultim_dia], (err, result) => {
@@ -215,21 +203,23 @@ const getGuardiesMesAny = (async (req, res) => {
     })
 });
 
-const generarGuardiesAny = (async (req , res) => {
-
-    
-
-
-});
 
 
 module.exports = { 
     getPlantilla, 
-    getTreballadors, 
-    getTreballador, 
+    getTreballadors,
+    getGuardiesMesAny,
+    getUnitats, 
+    getTreballador,
     getCategories, 
-    getGuardiesTreballador,
+    getGuardiesTreballador, 
+    getAgendaTreballador, 
+    getTorns, 
     getUnitats,
-    getGuardiesMesAny };
->>>>>>> Stashed changes
+    getHistorialTreballador,
+    getFestius,
+    updateFestiu, 
+    insertFestiu
+ };
+
 
--- api/routes/data-access.js
+++ api/routes/data-access.js
@@ -9,7 +9,6 @@ router.get('/treballador' , db_access.getTreballador)
 router.get('/categories' , db_access.getCategories)
 router.get('/unitats' , db_access.getUnitats)
 router.get('/guardiestreballador' , db_access.getGuardiesTreballador)
-<<<<<<< Updated upstream
 router.get('/getAgendaTreballador' , db_access.getGuardiesTreballador)
 router.get('/getHistorialTreballador' , db_access.getHistorialTreballador)
 router.get('/getTorns' , db_access.getTorns)
@@ -17,11 +16,9 @@ router.get('/getUnitats' , db_access.getUnitats)
 router.get('/getFestius' , db_access.getFestius)
 router.post('/updateFestiu', db_access.updateFestiu) 
 router.post('/insertFestiu', db_access.insertFestiu) 
-=======
 router.post('/guardiesperdata' , db_access.getGuardiesMesAny)
 
 
 
->>>>>>> Stashed changes
 
 module.exports = router
